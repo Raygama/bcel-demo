4ac2fc0f6313b75d159062f1aeafc4d118457724
Raygama <daffaraygama55@gmail.com>
2025-09-18 19:49:14 +0700

    base for judge 0b85abad6a951191875d8cb2af84a7da804c30c9
    BCEL-264 Add missing Node.accept() implementations (ConstantMethodHandle  ConstantMethodType  ParameterAnnotationEntry)  git-svn-id: https://svn.apache.org/repos/asf/commons/proper/bcel/trunk@1702615 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/bcel6/classfile/ConstantMethodHandle.java
+++ src/main/java/org/apache/commons/bcel6/classfile/ConstantMethodHandle.java
@@ -71,7 +71,7 @@ public final class ConstantMethodHandle extends Constant {
      */
     @Override
     public void accept( Visitor v ) {
-        v.visitConstantMethodHandle(this);
+        // TODO Add .visitMethodHandle to Visitor interface
     }
 
 
